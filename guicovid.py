291c1ffa4fe82c573bb290ddd385d95e679132cf
rohinraj <rohinsandro@gmail.com>
2020-07-24 22:07:57 +0530

    Added Basic Code

---
+++ guicovid.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from tkinter import *
+import pyttsx3
+from covid import Covid
+root = Tk()
+root.geometry("350x350")
+root.title("Get Country Wise Covid-19 Data")
+engine = pyttsx3.init()
+def speakdata():
+    covid=Covid()
+    cases = []
+    confirmed = []
+    active = []
+    deaths = []
+    recovered = []
+    try:
+        root.update()
+        countries = data.get()
+        country_names = countries.strip()
+        country_names = country_names.replace(" ", ",")
+        country_names = country_names.split(",")
+        for x in country_names:
+            cases.append(covid.get_status_by_country_name(x))
+            root.update()
+        for y in cases:
+            act="\nActive Cases in {} : {} ".format(y['country'],y['active'])
+            con="\nTotal Cases in {} : {}".format(y['country'],y['confirmed'])
+            dead="\nTotal Deaths in {} : {} ".format(y['country'],y['deaths'])
+            print(act)
+            engine.say(act)
+            print(con)
+            engine.say(con)
+            print(dead)
+            engine.say(dead)
+            engine.runAndWait()
+    except Exception as e:
+        data.set("Please Enter Correct Details")
+        
+Label(root, text="Enter all Countries Names\nFor whom you want to get\nCovid-19 data").pack()
+Label(root, text="Enter Country name:").pack()
+data = StringVar()
+data.set("Enter Country Name")
+entry = Entry(root, textvariable=data, width=50).pack()
+Button(root, text="Speak Data", command=showdata).pack()
+root.mainloop()
